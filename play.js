d05eee0003ccf1dfd3d8831f9b34310425397e72
Yifan Shi <stevenls1118@msn.com>
2022-10-19 20:38:27 -0600

    put connect function into client.js, added export and import

--- play.js
+++ play.js
@@ -1,17 +1,1 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-  conn.on("data", (serverData) => {
-    console.log(serverData);
-  })
-  return conn;
-};
-
-console.log("Connecting...");
-connect();
+const {connect} = require('./client');
