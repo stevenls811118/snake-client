43da3878ddef7842bc2474cd0bd0cffa0945e219
Yifan Shi <stevenls1118@msn.com>
2022-10-19 21:40:15 -0600

    write name to server

--- client.js
+++ client.js
@@ -1,19 +1,45 @@
 const net = require("net");
+/* const readline = require("readline");
+
+// create interface for input and output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const name = () => {
+  let name = "";
+
+  rl.question("What is your name\n", (input) => {
+    name = input.slice(0, 3);
+    console.log(`Your name is: ${name}`);
+    rl.close();
+  });
+  return name;
+}; */
 
 const connect = () => {
+
+  console.log("Connecting...");
+
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
 
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Connected to server');
+    conn.write(`Name: SYF`);
+  });
+
   conn.on("data", (serverData) => {
     console.log(serverData);
-  })
+  });
+  
+  
   return conn;
 };
-
-console.log("Connecting...");
-connect();
 
 module.exports = {connect};
