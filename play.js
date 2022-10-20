a1792abb5f919974aa805bc1b0cb692a07d33563
Yifan Shi <stevenls1118@msn.com>
2022-10-19 19:01:31 -0600

    play.js update for output the server message

---
+++ play.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+  conn.on("data", (serverData) => {
+    console.log(serverData);
+  })
+  return conn;
+};
+
+console.log("Connecting...");
+connect();
